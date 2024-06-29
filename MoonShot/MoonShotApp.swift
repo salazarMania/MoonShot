b0c2f224d13558dd039dd1785c2abc7e88216d6f
saniya <saniyaaaaakk@gmail.con>
2024-06-29 17:52:15 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by SANIYA KHATARKAR on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
